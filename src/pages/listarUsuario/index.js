f51cda4bb4fae7eb2b867063ead634cad5070e95
cisne negro <128532522+blackswnn@users.noreply.github.com>
2023-11-24 21:21:58 -0300

    alterado lista de usuarios

--- src/pages/listarUsuario/index.js
+++ src/pages/listarUsuario/index.js
@@ -4,14 +4,53 @@ import Menu from '../../componentes/menu'
 
 
 export default function Listausuario() {
+
+
+    // jason e apenas o colchete dentro das (chaves array) , suportando variavel e seu valor, assim identificando os itens mais facilmente.
+
+    const dados = [
+
+        { id: 1, nome: "Carlos", email: "carlos@gmail.com", senha: "123" },
+
+        { id: 2, nome: "Felipe", email: "carlos@gmail.com", senha: "321" },
+
+        { id: 3, nome: "Nilson", email: "carlos@gmail.com", senha: "321" },
+
+
+
+    ]
     return (
         <div className="dashboard-container">
             <div className='menu'>
-                <h1>Menu</h1>
-            <Menu/>
+                <Menu />
             </div>
             <div className='principal'>
+
                 <h1>Lista de Usuário</h1>
+
+                <table border={1}>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>.
+
+                        <th>Email</th>
+                        <th></th>
+                    </tr>
+
+                    {
+                        dados.map((usu)=>{
+                        return(
+                        <tr key={usu.toString()}>
+                            <td> {usu.id} </td>
+                            <td> {usu.nome} </td>
+                            <td> {usu.email} </td>
+
+                        </tr>
+                        )
+                        })
+                    }
+
+                </table>
             </div>
 
         </div>
